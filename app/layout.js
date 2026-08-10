e0ba5b28cac1441de0a827b0c978e7746c56dc10
jiliebaharin-star <jiliebaharin@gmail.com>
2026-08-10 11:26:14 +0800

    Add application layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'Lokasi Staf Harian',description:'Rekod keberadaan staf secara harian'};
+export default function RootLayout({children}){return <html lang="ms"><body>{children}</body></html>}
